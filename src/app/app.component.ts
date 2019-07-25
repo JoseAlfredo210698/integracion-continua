e9c368de0e195a8f7469c98be32414b369f7257f
JoseAlfredo210698 <163204@ids.upchiapas.edu.mx>
2019-07-25 13:56:43 -0500

    error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'título erroneo para probar la falla de la prueba';
+  title = 'título erroneo para probar la falla de la prueba...';
 }
 
 //https://notification-push-80f20.firebaseapp.com/
