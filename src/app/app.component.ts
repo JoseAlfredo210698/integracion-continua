9186f9157da15aaae784198adfafe58330e5b6c6
JoseAlfredo210698 <163204@ids.upchiapas.edu.mx>
2019-07-25 02:04:34 -0500

    test2 con error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'integracionContinua';
+  title  'integracionContinua';
 }
 
 //https://notification-push-80f20.firebaseapp.com/
